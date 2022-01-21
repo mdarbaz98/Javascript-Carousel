037a5088beea19a760f82ae50f20c4368dbcea1a
mdarbaz98 <zainshaikh2468@gmail.com>
2022-01-21 21:04:03 +0530

    added onload event listner

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ var random = document.getElementById("random");
 let currentItem = 0;
 
 // initial item
+window.addEventListener("load", showPerson(currentItem));
+
 window.addEventListener("DOMContentLoaded", function () {
   showPerson(currentItem);
 });
