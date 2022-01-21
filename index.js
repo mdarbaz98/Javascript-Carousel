e94f43252416c05d931a810446feee32c94a4d85
mdarbaz98 <zainshaikh2468@gmail.com>
2022-01-21 20:49:53 +0530

    added local images

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const data = [
   {
-    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    img: "./images/pexels-photo-220453.jpeg",
     name: "Mike",
     roll: "SEO",
     desc: "I am a SEO analyst as employee.",
   },
   {
-    img: "https://4bgowik9viu406fbr2hsu10z-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/Portrait_5-1.jpg",
+    img: "./images/Portrait_5-1.jpg",
     name: "Alex",
     roll: "Developer",
     desc: "I am a Web Developer at webenetic doing three months of internship",
   },
   {
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO3tVi_44g0p5HaJu1_MsgdEJ32IpWkpIWQQ&usqp=CAU",
+    img: "./images/images.jpg",
     name: "Angela",
     roll: "Market Executive",
     desc: "I am a Market Executive.",
   },
   {
-    img: "https://4bgowik9viu406fbr2hsu10z-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/Portrait_3.jpg",
+    img: "./images/Portrait_3.jpg",
     name: "Smith",
     roll: "Senior HR",
     desc: "I am a Senior HR at Octamicro  for last three year's.",
